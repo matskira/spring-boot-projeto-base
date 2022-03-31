40a287d70d839d833bd707c6fc9ee0a4d1993d47
Matheus Mariano Poda <matheus.poda@gmail.com>
2022-03-31 11:05:30 -0300

    Adicionado projeto spring boot inicial e injeção de dependência

---
+++ src/main/java/io/github/matskira/repository/ClienteRepository.java
@@ -0,0 +1,15 @@
+package io.github.matskira.repository;
+
+import org.springframework.stereotype.Repository;
+
+import io.github.matskira.model.Cliente;
+
+@Repository
+public class ClienteRepository {
+
+	public void persistir(Cliente cliente) {
+		
+	}
+
+	
+}
---
+++ src/main/java/io/github/matskira/service/ClienteService.java
@@ -0,0 +1,25 @@
+package io.github.matskira.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.matskira.model.Cliente;
+import io.github.matskira.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	private ClienteRepository repository;
+	
+	@Autowired
+	public ClienteService(ClienteRepository clienteRepository) {
+		this.repository = clienteRepository; 
+	};
+	
+	public void salvarCliente(Cliente cliente){
+		validaCliente(cliente);
+		this.repository.persistir(cliente);
+	};
+	
+	public void validaCliente(Cliente cliente){};
+}
